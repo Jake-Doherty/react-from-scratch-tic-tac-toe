085bb7422e332979a9582c6f67b7ab42190db387
Jake-Doherty <doherty.jacob.p@gmail.com>
2023-01-26 17:26:10 -0800

    imports box component and returns it in map through of board array passing the index value for that cell to the Box component

--- src/components/GameBoard/GameBoard.js
+++ src/components/GameBoard/GameBoard.js
@@ -1,15 +1,15 @@
 import React, { useContext } from 'react';
 import { GameContext } from '../../context/GameContext.js';
+import Box from '../Box/Box.js';
 import './GameBoard.css';
 
 export default function GameBoard() {
-  const { board, setBoard } = useContext(GameContext);
+  const { board } = useContext(GameContext);
+  console.log('board log', board);
   return (
     <div className="board">
-      {board.map((cell, index) => (
-        <div key={index} className="board-cell">
-          {cell}
-        </div>
+      {board.map((box, index) => (
+        <Box key={index} {...{ box }} />
       ))}
     </div>
   );
