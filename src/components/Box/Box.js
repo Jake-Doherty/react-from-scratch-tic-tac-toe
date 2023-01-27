913de24116c33eb6559f5aabc09a51f5d105b73b
Jake-Doherty <doherty.jacob.p@gmail.com>
2023-01-26 19:24:07 -0800

    adds click handler to box and passes index from board mapping to Box component

--- src/components/Box/Box.js
+++ src/components/Box/Box.js
@@ -1,6 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { GameContext } from '../../context/GameContext.js';
 
-export default function Box({ box }) {
+export default function Box({ box, index }) {
   // console.log('box props', { box });
-  return <div className="board-cell">{box}</div>;
+  const { active, board, setBoard, currentPlayer, setCurrentPlayer, setGameMessage } =
+    useContext(GameContext);
+
+  function handleChoice() {
+    if (!active) return;
+    if (box !== '') {
+      setGameMessage('This box is already taken!');
+      return;
+    }
+    setGameMessage('Who will win?');
+    board[index] = currentPlayer;
+    setBoard([...board]);
+    setCurrentPlayer(currentPlayer === 'X' ? 'O' : 'X');
+  }
+
+  return (
+    <div className="board-cell" onClick={handleChoice}>
+      {box}
+    </div>
+  );
 }
