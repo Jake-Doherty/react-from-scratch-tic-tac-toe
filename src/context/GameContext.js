546856752eee4b7d98da4140d2e0d09812b5839d
Jake-Doherty <doherty.jacob.p@gmail.com>
2023-01-26 17:24:46 -0800

    add static state values to array for testing

--- src/context/GameContext.js
+++ src/context/GameContext.js
@@ -3,7 +3,7 @@ const { createContext, useState } = require('react');
 const GameContext = createContext();
 
 const GameProvider = ({ children }) => {
-  const [board, setBoard] = useState(['', '', '', '', '', '', '', '', '']);
+  const [board, setBoard] = useState(['X', 'X', 'X', 'O', '', '', '', '', '']);
 
   return <GameContext.Provider value={{ board, setBoard }}>{children}</GameContext.Provider>;
 };
