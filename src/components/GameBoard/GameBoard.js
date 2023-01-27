eb91bdc769f3cef414589e0621e30956ea609600
Jake-Doherty <doherty.jacob.p@gmail.com>
2023-01-26 17:07:12 -0800

    import board state from GameContext and map through board array inserting an element to each index

--- src/components/GameBoard/GameBoard.js
+++ src/components/GameBoard/GameBoard.js
@@ -1,18 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { GameContext } from '../../context/GameContext.js';
 import './GameBoard.css';
 
 export default function GameBoard() {
+  const { board, setBoard } = useContext(GameContext);
   return (
     <div className="board">
-      <div className="board-cell"></div>
-      <div className="board-cell"></div>
-      <div className="board-cell"></div>
-      <div className="board-cell"></div>
-      <div className="board-cell"></div>
-      <div className="board-cell"></div>
-      <div className="board-cell"></div>
-      <div className="board-cell"></div>
-      <div className="board-cell"></div>
+      {board.map((cell, index) => (
+        <div key={index} className="board-cell">
+          {cell}
+        </div>
+      ))}
     </div>
   );
+}
+
+{
+  /* <div className="board-cell"></div> */
 }
