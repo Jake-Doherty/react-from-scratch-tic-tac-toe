fb5a11d0cb0666783b1e5d00a7397cb7c4948c01
Jake-Doherty <doherty.jacob.p@gmail.com>
2023-01-26 18:00:17 -0800

    adds current player state from GameContext to StatusBar

--- src/components/StatusBar/StatusBar.js
+++ src/components/StatusBar/StatusBar.js
@@ -2,10 +2,11 @@ import React, { useContext } from 'react';
 import { GameContext } from '../../context/GameContext.js';
 
 export default function StatusBar() {
-  const { gameMessage } = useContext(GameContext);
+  const { currentPlayer, gameMessage } = useContext(GameContext);
   return (
     <div className="status-bar">
       <h1>Tic-Tac-Toe</h1>
+      <h3>{`${currentPlayer}, it's your turn!`}</h3>
       <div className="status-bar__message">{gameMessage}</div>
     </div>
   );
