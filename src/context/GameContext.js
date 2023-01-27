2dde64a3b6bfd9861858be22a640230754182254
Jake-Doherty <doherty.jacob.p@gmail.com>
2023-01-26 17:55:05 -0800

    adds other needed state to GameContext

--- src/context/GameContext.js
+++ src/context/GameContext.js
@@ -4,8 +4,26 @@ const GameContext = createContext();
 
 const GameProvider = ({ children }) => {
   const [board, setBoard] = useState(['X', 'X', 'X', 'O', '', '', '', '', '']);
+  const [currentPlayer, setCurrentPlayer] = useState('X');
+  const [gameMessage, setGameMessage] = useState('Click a tile to start!');
+  const [active, setActive] = useState(true);
 
-  return <GameContext.Provider value={{ board, setBoard }}>{children}</GameContext.Provider>;
+  return (
+    <GameContext.Provider
+      value={{
+        board,
+        setBoard,
+        currentPlayer,
+        setCurrentPlayer,
+        gameMessage,
+        setGameMessage,
+        active,
+        setActive,
+      }}
+    >
+      {children}
+    </GameContext.Provider>
+  );
 };
 
 export { GameContext, GameProvider };
